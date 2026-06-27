864f1f373ef99b7b03cdcf18c9ad4fa7f7417a0b
Niteen Maurya <niteenmaurya21@gmail.com>
2026-06-28 00:21:55 +0530

    add doctor management menu

--- src/main/java/com/airtribe/meditrack/Main.java
+++ src/main/java/com/airtribe/meditrack/Main.java
@@ -1,29 +1,102 @@
 package com.airtribe.meditrack;
 
 import java.util.Scanner;
+
+import com.airtribe.meditrack.entity.Doctor;
+import com.airtribe.meditrack.entity.Specialization;
 import com.airtribe.meditrack.service.AppointmentService;
 import com.airtribe.meditrack.service.DoctorService;
 import com.airtribe.meditrack.service.PatientService;
 
-import java.util.Scanner;
-
 public class Main {
-    DoctorService doctorService = new DoctorService();
-    PatientService patientService = new PatientService();
-    AppointmentService appointmentService = new AppointmentService();
-    Scanner scanner = new Scanner(System.in);
+    private DoctorService doctorService = new DoctorService();
+    private PatientService patientService = new PatientService();
+    private AppointmentService appointmentService = new AppointmentService();
+    private Scanner scanner = new Scanner(System.in);
 
-    boolean running = true;
+    private boolean running = true;
 
-    while (running) {
-        //print menu
+    public static void main(String [] args ) {
+        Main app = new Main();
+        app.start();
+    }
+    public void start() {
+        while (running) {
+            showMenu();
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    addDoctor();
+                    break;
 
-        int choice = scanner.nextInt();
+                case 2:
+                    doctorService.viewAllDoctors();
+                    break;
 
-        switch(choice) {
-            case 1 :
+                case 9:
+                    running = false;
+                    System.out.println("Thank you.");
+                    break;
 
+                default:
+                    System.out.println("Invalid choice.");
+
+
+            }
         }
     }
+
+    private void addDoctor() {
+
+        System.out.print("Enter Doctor ID: ");
+        String id = scanner.next();
+
+        System.out.print("Enter Doctor Name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter Age: ");
+        int age = scanner.nextInt();
+
+        // Read specialization and fee...
+        System.out.print("Enter Specialization (CARDIOLOGIST, DERMATOLOGIST, NEUROLOGIST, GENERAL_PHYSICIAN): ");
+        String input = scanner.next().toUpperCase();
+
+        Specialization specialization = Specialization.valueOf(input);
+
+        System.out.print("Enter Consultation Fee: ");
+        double fee = scanner.nextDouble();
+
+        Doctor doctor = new Doctor(id, name, age, specialization, fee);
+
+        doctorService.addDoctor(doctor);
+
+        System.out.println("Doctor added successfully!");
+    }
+
+
+
+    private void showMenu() {
+
+        System.out.println("\n===== MEDITRACK =====");
+
+        System.out.println("1. Add Doctor");
+        System.out.println("2. View Doctors");
+
+        System.out.println("3. Add Patient");
+        System.out.println("4. View Patients");
+
+        System.out.println("5. Book Appointment");
+        System.out.println("6. View Appointments");
+
+        System.out.println("7. Search Appointment");
+
+        System.out.println("8. Remove Appointment");
+
+        System.out.println("9. Exit");
+
+        System.out.print("Enter Choice : ");
+
+    }
+
 
 }
